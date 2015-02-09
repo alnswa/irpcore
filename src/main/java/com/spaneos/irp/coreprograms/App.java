40f994fd69116bcbc474bcb21ed64887f379225d
IRP <IRP@192.168.1.207>
2015-02-09 17:07:16 +0530

    Code is added to accept user name by Lakshman

--- src/main/java/com/spaneos/irp/coreprograms/App.java
+++ src/main/java/com/spaneos/irp/coreprograms/App.java
@@ -1,5 +1,7 @@
 package com.spaneos.irp.coreprograms;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("Enter your name :");
+        String name=new Scanner(System.in).nextLine();
+        System.out.println("Entered name is: "+name);
+        
     }
+    
+    
 }
